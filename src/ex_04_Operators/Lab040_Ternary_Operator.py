10d1b73b5c0a96042ecdc8926fe8177954c5fc1f
Sumitkumargavaskar <sumitkumargavaskar@gmail.com>
2026-01-14 20:31:13 +0530

    14th jan 2025

---
+++ src/ex_04_Operators/Lab040_Ternary_Operator.py
@@ -0,0 +1,11 @@
+# Ternary Operator:  if-else statement.
+x = 10
+y = 20
+
+
+print("x is grater than y" if x > y else "x is less than y")
+
+#if x > y:
+#    print("x is grater")
+#else:
+#    print("y is grater")
