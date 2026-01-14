f925421f23456713b6f5463a96cbe44dbe97284a
Sumitkumargavaskar <sumitkumargavaskar@gmail.com>
2026-01-14 19:22:14 +0530

    14th jan 2025

---
+++ src/ex_03_Literals_Variables/Lab029_Task1.py
@@ -0,0 +1,21 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+num1 = float(input("Enter a number"))
+num2 = float(input("Enter another number"))
+num3 = float(input("Enter a third number"))
+
+
+
+# Performing operations
+sub_result = num1 - num2 - num3
+sub_result4 = num1 + num2 + num3
+print(sub_result)  # Subtracting second number from first
+print(sub_result4)  # Sub third number from second
+
+mul_result = num1 * num2  * num3 # Multiplying all three numbers
+print(mul_result)
+
+divisor_result = num1 // num2 // num3
+print(divisor_result)
